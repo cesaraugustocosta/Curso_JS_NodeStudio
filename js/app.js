36f7e699581cd4b9a2c095333c114c8e6394ec92
cesaraugustocosta <cesaraug76@hotmail.com>
2023-11-25 11:28:54 -0300

    Enviando alterações do projeto

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// window.alert("Olá, Mundo JS!")
+
+// console.log(document.getElementById("titulo").innerHTML )
+
+//location.href("https://google.com.br")
+
+/*Exercicio 02
+function voltar(){
+    history.back()
+}
+
+function adiantar(){
+    history.forward()
+}
+*/
+console.log(
+    document.getElementsByClassName("carro")
+)
+
+
